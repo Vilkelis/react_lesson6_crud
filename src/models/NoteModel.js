867e3698be506e9136d4d062bf30237386bf8ae7
Stepan Golovanov <vilkelis@yandex.ru>
2020-05-03 17:07:20 +0500

    Home work for lesson 6 of Netology training course: CRUD

---
+++ src/models/NoteModel.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+function NoteModel() {
+  return PropTypes.shape({ id: PropTypes.number.isRequired,
+                           content: PropTypes.string.isRequired                            
+                         });
+}
+  
+export default NoteModel;
